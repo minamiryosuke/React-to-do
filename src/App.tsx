1aaeb82fac58f82715c28ab900a09342932b1ec8
ryosuke minami <curovir89oy@gmail.com>
2025-04-15 21:18:27 +0900

    layoutを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-
-import React from 'react';
-import TodoPage from './pages/TodoPage'; 
+import React from "react";
+import TodoPage from "./pages/TodoPage";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Report from "./pages/Report";
+import NoMatch from "./pages/NoMatch";
+import AppLayout from "./components/layput/AppLayout";
 
 function App() {
   return (
-    <div className="App">
-      <TodoPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/report" element={<Report />} />
+          {/** 存在しないURLが入力された時の表示 */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
